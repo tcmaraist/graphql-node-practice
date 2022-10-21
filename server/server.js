ea29efde6da88235d2e72eb8c04a59496ed45363
Ty Maraist <tcmaraist@gmail.com>
2022-10-21 13:57:29 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Hello GraphQL World!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server
+  .listen({ port: 9000 })
+  .then((serverInfo) => console.log(`Server running at ${serverInfo.url}`));
